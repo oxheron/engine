c530a8daa619471fbd455e1143bbc4a5f79b8596
0xheron <micahogden8@gmail.com>
2024-02-03 20:31:59 -0800

    Set vertex count works now

--- src/framebuffer/framebuffer.cpp
+++ src/framebuffer/framebuffer.cpp
@@ -22,7 +22,7 @@ bgfx::Encoder* render_framebuffer(bgfx::FrameBufferHandle framebuffer,
     if (!encoder) encoder = bgfx::begin();
 
     encoder->setTexture(stage, handle, getTexture(framebuffer, attachment));
-    draw_vertex_count(3, encoder);
+    encoder->setVertexCount(3);
 
     return encoder; 
 }
